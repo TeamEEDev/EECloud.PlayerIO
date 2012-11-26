3ac91c8f5e60d434f33978cc9b1e848612f99284
Jo Jatekok <jojatekokweb@gmail.com>
2012-11-26 19:40:35 +0100

    Started working on some stuff...

--- EECloud.PlayerIO/Helpers/Message.cs
+++ EECloud.PlayerIO/Helpers/Message.cs
@@ -1,4 +1,6 @@
-﻿namespace EECloud.PlayerIO
+﻿using System.Collections.Generic;
+
+namespace EECloud.PlayerIO
 {
     /// <summary>
     /// Represents a message sent between the client and the server.
@@ -6,5 +8,21 @@
     /// </summary>
     public class Message
     {
+        public string Type;
+        private readonly Dictionary<object, byte> parameters = new Dictionary<object, byte>();
+
+        public uint Count
+        {
+            get { return (uint)parameters.Count; }
+        }
+
+        /// <summary>Creates a new Message.</summary>
+        /// <param name="type">The type of message to create.</param>
+        /// <param name="parameters">A list of the data to add to the message.</param>
+        /// <returns></returns>
+        public static Message Create(string type, params object[] parameters)
+        {
+            
+        }
     }
 }

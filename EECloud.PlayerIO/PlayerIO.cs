5c68246767ff517d4030d2583cb0b54f9213aa33
Jo Jatekok <jojatekokweb@gmail.com>
2012-12-01 12:37:35 +0100

    It's more elegant like that

--- EECloud.PlayerIO/PlayerIO.cs
+++ EECloud.PlayerIO/PlayerIO.cs
@@ -51,61 +51,64 @@ namespace EECloud.PlayerIO
             return unixTime + ":" + BitConverter.ToString(hmac).Replace("-", "").ToLowerInvariant();
         }
 
-        /// <summary>
-        /// Connects to a game based on Player.IO as a Facebook user using a Facebook access token.
-        /// </summary>
-        /// <param name="gameId">The ID of the game you wish to connect to. This value can be found in the admin panel.</param>
-        /// <param name="accessToken">The Facebook access token of the user you wish to authenticate.</param>
-        /// <returns>A new instance of Client if logging in was successful.</returns>
-        public static Client FacebookOAuthConnect(string gameId, string accessToken)
+        public static class QuickConnect
         {
-            var facebookConnectArg = new FacebookOAuthConnectArgs {GameId = gameId, AccessToken = accessToken};
-            var facebookConnectOutput =
-                Channel.Request<FacebookOAuthConnectArgs, ConnectOutput, PlayerIOError>(418,
-                                                                                        facebookConnectArg);
-            return new Client(Channel, facebookConnectOutput.Token, facebookConnectOutput.UserId);
-        }
-
-        /// <summary>
-        /// Connects to a game based on Player.IO as a simple user.
-        /// </summary>
-        /// <param name="gameId">The ID of the game you wish to connect to. This value can be found in the admin panel.</param>
-        /// <param name="usernameOrEmail">The username or e-mail address of the user you wish to authenticate.</param>
-        /// <param name="password">The password of the user you wish to authenticate.</param>
-        /// <returns>A new instance of Client if logging in was successful.</returns>
-        public static Client SimpleConnect(string gameId, string usernameOrEmail, string password)
-        {
-            var simpleConnectArg = new SimpleConnectArgs
-                                       {
-                                           GameId = gameId,
-                                           UsernameOrEmail = usernameOrEmail,
-                                           Password = password
-                                       };
-            var simpleConnectOutput =
-                Channel.Request<SimpleConnectArgs, ConnectOutput, PlayerIOError>(400,
-                                                                                 simpleConnectArg);
-            return new Client(Channel, simpleConnectOutput.Token, simpleConnectOutput.UserId);
-        }
-
-        /// <summary>
-        /// Connects to a game based on Player.IO as a Kongregate user.
-        /// </summary>
-        /// <param name="gameId">The ID of the game you wish to connect to. This value can be found in the admin panel.</param>
-        /// <param name="userId">The Kongregate user ID of the user you wish to authenticate.</param>
-        /// <param name="gameAuthToken">The Kongregate auth token of the game you wish to connect to (depends on the user you wish to authenticate).</param>
-        /// <returns>A new instance of Client if logging in was successful.</returns>
-        public static Client KongregateConnect(string gameId, string userId, string gameAuthToken)
-        {
-            var kongregateConnectArg = new KongregateConnectArgs
+            /// <summary>
+            /// Connects to a game based on Player.IO as a simple user.
+            /// </summary>
+            /// <param name="gameId">The ID of the game you wish to connect to. This value can be found in the admin panel.</param>
+            /// <param name="usernameOrEmail">The username or e-mail address of the user you wish to authenticate.</param>
+            /// <param name="password">The password of the user you wish to authenticate.</param>
+            /// <returns>A new instance of Client if logging in was successful.</returns>
+            public static Client SimpleConnect(string gameId, string usernameOrEmail, string password)
             {
-                GameId = gameId,
-                UserId = userId,
-                GameAuthToken = gameAuthToken
-            };
-            var kongregateConnectOutput =
-                Channel.Request<KongregateConnectArgs, ConnectOutput, PlayerIOError>(400,
-                                                                                     kongregateConnectArg);
-            return new Client(Channel, kongregateConnectOutput.Token, kongregateConnectOutput.UserId);
+                var simpleConnectArg = new SimpleConnectArgs
+                                           {
+                                               GameId = gameId,
+                                               UsernameOrEmail = usernameOrEmail,
+                                               Password = password
+                                           };
+                var simpleConnectOutput =
+                    Channel.Request<SimpleConnectArgs, ConnectOutput, PlayerIOError>(400,
+                                                                                     simpleConnectArg);
+                return new Client(Channel, simpleConnectOutput.Token, simpleConnectOutput.UserId);
+            }
+
+            /// <summary>
+            /// Connects to a game based on Player.IO as a Facebook user using a Facebook access token.
+            /// </summary>
+            /// <param name="gameId">The ID of the game you wish to connect to. This value can be found in the admin panel.</param>
+            /// <param name="accessToken">The Facebook access token of the user you wish to authenticate.</param>
+            /// <returns>A new instance of Client if logging in was successful.</returns>
+            public static Client FacebookOAuthConnect(string gameId, string accessToken)
+            {
+                var facebookConnectArg = new FacebookOAuthConnectArgs { GameId = gameId, AccessToken = accessToken };
+                var facebookConnectOutput =
+                    Channel.Request<FacebookOAuthConnectArgs, ConnectOutput, PlayerIOError>(418,
+                                                                                            facebookConnectArg);
+                return new Client(Channel, facebookConnectOutput.Token, facebookConnectOutput.UserId);
+            }
+
+            /// <summary>
+            /// Connects to a game based on Player.IO as a Kongregate user.
+            /// </summary>
+            /// <param name="gameId">The ID of the game you wish to connect to. This value can be found in the admin panel.</param>
+            /// <param name="userId">The Kongregate user ID of the user you wish to authenticate.</param>
+            /// <param name="gameAuthToken">The Kongregate auth token of the game you wish to connect to (depends on the user you wish to authenticate).</param>
+            /// <returns>A new instance of Client if logging in was successful.</returns>
+            public static Client KongregateConnect(string gameId, string userId, string gameAuthToken)
+            {
+                var kongregateConnectArg = new KongregateConnectArgs
+                {
+                    GameId = gameId,
+                    UserId = userId,
+                    GameAuthToken = gameAuthToken
+                };
+                var kongregateConnectOutput =
+                    Channel.Request<KongregateConnectArgs, ConnectOutput, PlayerIOError>(400,
+                                                                                         kongregateConnectArg);
+                return new Client(Channel, kongregateConnectOutput.Token, kongregateConnectOutput.UserId);
+            }
         }
     }
 }

c7575b5fdcf181d5b2c96a64bc88291a88db4fc7
Jo Jatekok <jojatekokweb@gmail.com>
2012-12-02 14:28:57 +0100

    Added the ProtocolType enum

---
+++ EECloud.PlayerIO/Helpers/ProtocolType.cs
@@ -0,0 +1,11 @@
+﻿namespace EECloud.PlayerIO
+{
+    internal enum ProtocolType : byte
+    {
+        Binary,
+        Http = 71,
+        Auto = 255,
+        WebsocketRfc6544Binary = 11,
+        WebsocketV76Base64
+    }
+}

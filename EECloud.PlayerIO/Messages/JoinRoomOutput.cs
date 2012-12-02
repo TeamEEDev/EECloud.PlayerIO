79f36db2df2b21544489e03cb124674168c5b9ec
Jo Jatekok <jojatekokweb@gmail.com>
2012-12-02 19:43:52 +0100

    Fixed a bug; added JoinRoom()

---
+++ EECloud.PlayerIO/Messages/JoinRoomOutput.cs
@@ -0,0 +1,22 @@
+﻿using ProtoBuf;
+
+namespace EECloud.PlayerIO.Messages
+{
+    [ProtoContract]
+    public class JoinRoomOutput
+    {
+        [ProtoMember(1)]
+        public string JoinKey
+        {
+            get;
+            set;
+        }
+
+        [ProtoMember(2)]
+        public ServerEndpoint[] Endpoints
+        {
+            get;
+            set;
+        }
+    }
+}

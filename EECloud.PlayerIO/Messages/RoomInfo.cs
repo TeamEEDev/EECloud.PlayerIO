0af932e8c5f3d3e7c1d48bc2d064d545a3a52654
Jo Jatekok <jojatekokweb@gmail.com>
2012-12-03 14:48:08 +0100

    Added the RoomInfo message

---
+++ EECloud.PlayerIO/Messages/RoomInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace EECloud.PlayerIO
+{
+    public class RoomInfo
+    {
+        public string Id
+        {
+            get;
+            private set;
+        }
+
+        public string RoomType
+        {
+            get;
+            private set;
+        }
+
+        public int OnlineUsers
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>The current room data for the room.</summary>
+        public Dictionary<string, string> RoomData
+        {
+            get;
+            private set;
+        }
+
+        internal RoomInfo(string id, string roomType, int onlineUsers, Dictionary<string, string> roomData)
+        {
+            Id = id;
+            RoomType = roomType;
+            OnlineUsers = onlineUsers;
+            RoomData = (roomData ?? new Dictionary<string, string>());
+        }
+    }
+}
